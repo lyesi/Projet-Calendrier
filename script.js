0c616676f3048c76fee19920943ef060a83a0bc6
lyesi <astrophilyes@gmail.com>
2020-10-01 09:53:30 +0200

    Ajout du menu burger(+ responsive) + document image

---
+++ script.js
@@ -0,0 +1,6 @@
+function onClickMenu() {
+    document.getElementById("burgerMenuBar").classList.toggle("change");
+    document.getElementById("navigation").classList.toggle("change");
+    document.getElementById("burgerMenuCircle").classList.toggle("changeCircle");
+   //document.getElementById("body").classList.toggle("changeBody");
+}
